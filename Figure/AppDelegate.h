2a80bf593bceead893ecfaaab758de0dc36a283f
Administrator <administrator@macminissu2.training.local>
2015-09-07 12:43:32 +0300

    Initial Commit

---
+++ Figure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Figure
+//
+//  Created by Administrator on 9/7/15.
+//  Copyright (c) 2015 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
