0b5280ae0636e1065565d723a940f1f59fc39f65
Administrator <administrator@macminissu2.training.local>
2015-09-07 12:56:22 +0300

    Start of project

---
+++ Figure/Drawing.h
@@ -0,0 +1,16 @@
+//
+//  Drawing.h
+//  Figure
+//
+//  Created by Administrator on 9/7/15.
+//  Copyright (c) 2015 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Drawing : UIView
+
+typedef enum {triangular, circle} FigureType;
+
+
+@end
---
+++ Figure/Drawing.m
@@ -0,0 +1,19 @@
+//
+//  Drawing.m
+//  Figure
+//
+//  Created by Administrator on 9/7/15.
+//  Copyright (c) 2015 Administrator. All rights reserved.
+//
+
+#import "Drawing.h"
+
+@implementation Drawing
+
+- (void)drawRect:(CGRect)Rect {
+
+    
+    // Drawing code here.
+}
+
+@end
